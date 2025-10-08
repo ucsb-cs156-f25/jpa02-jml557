81b7ad3bc0ff6d2a72260c8248cc63d672fb1bea
Julia Lin <124005009+jml557@users.noreply.github.com>
2025-10-08 12:39:53 -0700

    jml - updated test coverage

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,10 +9,12 @@ import org.junit.jupiter.api.Test;
 public class TeamTest {
 
     Team team;
+    Team team_same;
+    Team team_diff_name;
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");   
     }
 
     @Test
@@ -19,8 +22,41 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+   @Test 
+   public void toString_returns_correct_string() {
+    assertEquals("Team(name=test-team, members=[])", team.toString());
+   }
+
+   @Test 
+   public void equals_same_object() {
+    assertEquals(true, team.equals(team));
+   }
+
+   @Test 
+   public void equals_different_class() {
+    String s = "wassup";
+    assertEquals(false, team.equals(s));
+   }
+
+   @Test 
+   public void equals_members() {
+    team_same = new Team("test-team");
+    assertTrue(team.equals(team_same), "teams should be equal");
+    team_same.addMember("person");
+    assertTrue(!(team.equals(team_same)), "teams contain different members");
+
+    team_diff_name = new Team("test_dream");
+    assertTrue(!(team.equals(team_diff_name)), "teams have different names");
+   }
+
+   @Test 
+   public void hash_code_is_good() {
+    team_same = new Team("test-team");
+    int actual = team.hashCode();
+    int expected = -1226298695;
+
+    assertEquals(actual, expected);
+    assertEquals(team_same.hashCode(), team.hashCode());
+   }
 
 }
